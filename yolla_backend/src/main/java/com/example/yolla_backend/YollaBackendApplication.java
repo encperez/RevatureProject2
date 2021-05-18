4503926c671db9234eb4cf02d5f86f278ebbdb45
encperez <encperez06@gmail.com>
2021-05-18 14:33:03 -0500

    Initializing Project

---
+++ yolla_backend/src/main/java/com/example/yolla_backend/YollaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.yolla_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YollaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YollaBackendApplication.class, args);
+	}
+
+}
