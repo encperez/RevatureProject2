4744d4ff2996c8d45ca0b3447ca36a0d8977fd2a
encperez <encperez06@gmail.com>
2021-05-28 05:38:49 -0500

    Added conditional rendering for the bookmark and sidebar.

--- yolla_frontend/src/components/sidebar.component.js
+++ yolla_frontend/src/components/sidebar.component.js
@@ -9,7 +9,7 @@ export default class Sidebar extends Component {
     static contextType = GlobalContext
     constructor(props) {
         super(props);
-        this.state = { bookmarks: [], words: [], wordsOpen: false, bookmarkOpen: false };
+        this.state = { bookmarks: [], words: [], wordsOpen: false, bookmarkOpen: false, page: "/" };
         this.handleWordClick = this.handleWordClick.bind(this);
         this.handleInterval = this.handleInterval.bind(this);
     }
@@ -20,17 +20,15 @@ export default class Sidebar extends Component {
     handleInterval(){
         let wordResponse;
         let bookmarkResponse;
-        console.log(window.location.pathname);
+        this.setState({page: window.location.pathname});
         if(window.location.pathname == "/home"){
             axios.get('http://localhost:8080/user/' + sessionStorage.getItem("myID")).then(res=>{
                 wordResponse = Object.keys(res.data.words);
-                console.log(wordResponse);
                 this.setState({words: wordResponse});
             });
 
             axios.get('http://localhost:8080/bookmark/' + sessionStorage.getItem("myUser")).then(res=>{
                 bookmarkResponse = res.data;
-                console.log(bookmarkResponse);
                 this.setState({bookmarks: bookmarkResponse});
             })
         }
@@ -51,7 +49,9 @@ export default class Sidebar extends Component {
         const{wordsOpen} = this.state;
         const{bookmarksOpen} = this.state;
         return (
+        
         <React.Fragment>
+            {window.location.pathname == "/home" &&
             <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
             <CDBSidebar textColor="#fff" backgroundColor="#333">
             <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
@@ -124,7 +124,9 @@ export default class Sidebar extends Component {
                 </CDBSidebarFooter>
             </CDBSidebar>
             </div>
+            }
         </React.Fragment>
-        );
+                );
+    
     }
 }
